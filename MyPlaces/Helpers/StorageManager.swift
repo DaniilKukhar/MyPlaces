6038b11e592995d8a89f7e5bdde6be56d7fba70a
Daniil <kukhardania@gmail.com>
2022-03-23 14:54:53 +0200

    Initial Commit

---
+++ MyPlaces/Helpers/StorageManager.swift
@@ -0,0 +1,32 @@
+//
+//  StorageManager.swift
+//  MyPlaces
+//
+//  Created by Alexey Efimov on 07/12/2018.
+//  Copyright © 2018 Alexey Efimov. All rights reserved.
+//
+
+import RealmSwift
+
+let realm = try! Realm()
+
+class StorageManager {
+    
+    static func saveObject(_ place: Place) {
+        
+        try! realm.write {
+            realm.add(place)
+        }
+    }
+    
+    static func deleteObject(_ place: Place) {
+        
+        try! realm.write {
+            realm.delete(place)
+        }
+    }
+    
+    deinit {
+        print("deinit", StorageManager.self)
+    }
+}
